3388d35ad7d9d2e039cfc40df21774041cd2d934
satyapraveena <30664876+satyapraveena@users.noreply.github.com>
2017-08-15 15:12:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
 var articles = {
+ article_one : {
     title: "article_one",
     heading:"First Article",
     link1 :"/",
@@ -15,6 +15,29 @@ var articles = {
         <p>welcome to imad..you are going to experience the coding world</p>
     </div>
     <div>Aug 8,2017</div>`
+},
+article_two : {
+    title: "article_two",
+    heading:"Second Article",
+    link1 :"/article_one",
+    link2:"/article_three",
+    content : `
+    <div>
+        <p>Let us start..this is a html file which can be understood by browser</p>
+    </div>
+    <div>Aug 8,2017</div>`
+},
+article_three : {
+    title: "article_three",
+    heading:"Third Article",
+    link1 :"/article_two",
+    link2:"/",
+    content : `
+    <div>
+        <p>css file is to style the content to be displayed</p>
+    </div>
+    <div>Aug 8,2017</div>`
+}
 }
 var display = function(obj){
     var title = obj.title;
@@ -54,21 +77,21 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-//app.get('/:articlename', function (req, res) {
-//  res.send(display(articles[articlename]));
+app.get('/:articlename', function (req, res) {
+  res.send(display(articles[articlename]));
+});
+//app.get('/article_one',function(req,res)
+//{
+// res.send(display(articles));
 //});
-app.get('/article_one',function(req,res)
-{
- res.send(display(articles));
-});
-app.get('/article_two',function(req,res)
-{
-  res.sendFile(path.join(__dirname,'ui','article_two.html'));
-});
-app.get('/article_three',function(req,res)
-{
-  res.sendFile(path.join(__dirname,'ui','article_three.html'));
-});
+//app.get('/article_two',function(req,res)
+//{
+//  res.sendFile(path.join(__dirname,'ui','article_two.html'));
+//});
+//app.get('/article_three',function(req,res)
+//{
+//  res.sendFile(path.join(__dirname,'ui','article_three.html'));
+//});
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 

868e61fb6928b509e52f1050f64de3117ba03463
satyapraveena <30664876+satyapraveena@users.noreply.github.com>
2017-08-15 15:48:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/:articlename', function (req, res) {
-  res.send(display(articles[articlename]));
+app.get('/:articleName', function (req, res) {
+  res.send(display(articles[articleName]));
 });
 //app.get('/article_one',function(req,res)
 //{
